a7b3d849ceec6498c99f07898a2edef0414262e2
Neaz-mq <neazmorshed666@gmail.com>
2025-04-19 13:18:02 +0600

    work services

--- src/pages/Home/Services/Services.jsx
+++ src/pages/Home/Services/Services.jsx
@@ -5,7 +5,7 @@ const Services = () => {
         <div className='min-h-screen'>
             <div className="bg-white w-[90rem] h-[70rem] left-[17rem] -mt-[29rem] rounded-[120px] mb-80 z-50 absolute ">
                 {/* You can add content inside this div */}
-                <div className='mt-10 flex justify-center items-center text-center px-4'>
+                <div className='mt-14 flex justify-center items-center text-center px-4'>
                     <h1 className='text-[#888888] text-base font-normal leading-relaxed max-w-2xl'>
                         At <span className='font-medium'>[Restaurant Name]</span>, we don’t just serve food—
                         we create unforgettable dining experiences. From the moment you step through our doors.
