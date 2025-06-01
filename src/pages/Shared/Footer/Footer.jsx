cc1f10c8eaafe032297a40bc7cdef9143a3d5424
Neaz-mq <neazmorshed666@gmail.com>
2025-06-01 13:09:37 +0600

    working container

--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -12,31 +12,31 @@ const Footer = () => {
     <footer className="bg-[#2C6252] text-white">
      <div className="container mx-auto">
  {/* Top Image */}
-      <div className="flex justify-center items-center pt-10 3xl:ml-60 2xl:ml-60 xl:ml-60 lg:ml-44">
+      <div className="flex justify-center items-center pt-10 3xl:ml-[30rem] 2xl:ml-[30rem] xl:ml-[30rem] lg:ml-[23rem]">
         <img
           src="https://res.cloudinary.com/dxohwanal/image/upload/v1747630031/Mask_Group_23_pyld0k.png"
           alt="Footer Background"
-          className="w-full 3xl:max-w-[800px] 2xl:max-w-[800px] xl:max-w-[800px] lg:max-w-[790px] h-auto object-cover "
+          className="w-full 3xl:max-w-[1100px] 2xl:max-w-[750px] xl:max-w-[650px] lg:max-w-[550px] 3xl:h-[15rem] 2xl:h-[12rem] object-cover "
         />
       </div>
 
       {/* Middle Section */}
-      <div className="max-w-[1152px] mx-auto px-6 py-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 text-left">
+      <div className="max-w-[1152px] mx-auto px-6 py-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 3xl:gap-28 2xl:gap-0 3xl:text-left">
         {/* Logo */}
-        <div className="flex items-start">
+        <div className="flex items-start container mx-auto lg:ml-60 xl:ml-72 2xl:ml-56 3xl:ml-0 ">
           <a href="/">
-            <div className="bg-[#3A7563] px-6 py-4 flex items-center space-x-2 w-fit mt-2">
-              <img src="/logo.svg" alt="Cuisine Logo" className="w-6 h-6" />
-              <span className="font-bold text-lg">Cuisine</span>
+            <div className="bg-[#3A7563] px-6 py-4 flex items-center space-x-2 w-fit 3xl:mt-2 2xl:mt-2 xl:mt-1 lg:mt-1 ">
+              <img src="/logo.svg" alt="Cuisine Logo" className="3xl:w-6 3xl:h-6 2xl:w-6 2xl:h-6 xl:w-6 xl:h-6 lg:w-3 lg:h-3 " />
+              <span className="font-bold 3xl:text-lg 2xl:text-lg xl:text-lg lg:text-sm">Cuisine</span>
             </div>
           </a>
         </div>
 
         {/* Customer Services */}
         
-        <div className="lg:-ml-8 3xl:-ml-0 2xl:-ml-0 xl:-ml-0">
-          <h2 className="font-semibold mb-4 text-lg">Customer Services</h2>
-          <ul className="space-y-2 text-sm font-thin">
+        <div className=" 3xl:-ml-14 2xl:ml-36 xl:ml-48 lg:ml-40">
+          <h2 className="font-semibold mb-4 3xl:text-lg 2xl:text-lg xl:text-base lg:text-[13px] whitespace-nowrap">Customer Services</h2>
+          <ul className="space-y-2 3xl:text-sm 2xl:text-sm xl:text-[10px] lg:text-[9px] font-thin">
             <li>My Account</li>
             <li>Track Your Order</li>
             <li>Return</li>
@@ -49,9 +49,9 @@ const Footer = () => {
         </div>
 
         {/* Our Information */}
-        <div className="lg:-ml-8 3xl:-ml-0 2xl:-ml-0 xl:-ml-0">
-          <h2 className="font-semibold mb-4 text-lg">Our Information</h2>
-          <ul className="space-y-2 text-sm font-thin">
+        <div className="lg:ml-32 3xl:-ml-10 2xl:ml-36 xl:ml-40">
+          <h2 className="font-semibold mb-4 3xl:text-lg 2xl:text-lg xl:text-base lg:text-[13px] whitespace-nowrap">Our Information</h2>
+          <ul className="space-y-2 3xl:text-sm 2xl:text-sm xl:text-[10px] lg:text-[9px] font-thin">
             <li>Privacy</li>
             <li>User Terms & Condition</li>
             <li>
@@ -63,9 +63,9 @@ const Footer = () => {
         </div>
 
         {/* Contact Info */}
-        <div className="lg:-ml-8 3xl:-ml-0 2xl:-ml-0 xl:-ml-0">
-          <h2 className="font-semibold mb-4 text-lg">Contact Info</h2>
-          <ul className="space-y-2 text-sm font-thin">
+        <div className="lg:ml-20 3xl:-ml-4 2xl:ml-36 xl:ml-36">
+          <h2 className="font-semibold mb-4 3xl:text-lg 2xl:text-lg xl:text-base lg:text-[13px]">Contact Info</h2>
+          <ul className="space-y-2 3xl:text-sm 2xl:text-sm xl:text-[10px] lg:text-[9px] font-thin">
             <li className="flex items-center gap-2">
               <FaPhone />
               <span className="underline">+0123-456-789</span>
@@ -89,10 +89,10 @@ const Footer = () => {
 
       {/* Bottom Bar */}
       <div className="bg-[#FF4C15] w-full">
-        <div className="max-w-[1152px] mx-auto flex flex-col md:flex-row justify-between items-center px-6 py-4 text-sm text-white gap-4">
+        <div className="max-w-[1150px] mx-auto flex flex-col md:flex-row justify-between items-center px-6 py-4 text-sm text-white gap-4">
           <div className="flex items-center gap-2">
             <FaRegCopyright />
-            <span>2024 Grocery Website Design. All Rights Reserved.</span>
+            <span>2025 Grocery Website Design. All Rights Reserved.</span>
           </div>
           <div className="flex items-center gap-4">
             <span>English</span>
--- src/pages/Shared/Topbar/Topbar.jsx
+++ src/pages/Shared/Topbar/Topbar.jsx
@@ -131,59 +131,58 @@ const TopBar = () => {
     return (
         <div className='overflow-hidden container mx-auto '>
             <div className="flex flex-col sm:flex-row items-center bg-white px-4 sm:px-6 text-gray-700 text-sm relative z-0 3xl:ml-0 2xl:ml-10 xl:ml-8 lg:ml-0">
-            {/* Background Image */}
-            <img
-                src="https://res.cloudinary.com/dxohwanal/image/upload/v1742451276/Group_22_fwl6sw.png"
-                alt="Group 22"
-                className="w-full sm:w-auto h-auto sm:h-[12rem] md:h-[18rem] lg:h-[22rem] xl:h-[23rem] 2xl:h-[24rem] 3xl:h-[25rem] -mt-48 sm:-mt-[6rem] md:-mt-[8.2rem] lg:-mt-[13rem] sm:ml-0 3xl:ml-16 lg:-ml-12 md:-ml-4"
-            />
+                {/* Background Image */}
+                <img
+                    src="https://res.cloudinary.com/dxohwanal/image/upload/v1742451276/Group_22_fwl6sw.png"
+                    alt="Group 22"
+                    className="w-full sm:w-auto h-auto sm:h-[12rem] md:h-[18rem] lg:h-[22rem] xl:h-[23rem] 2xl:h-[24rem] 3xl:h-[25rem] -mt-48 sm:-mt-[6rem] md:-mt-[8.2rem] lg:-mt-[13rem] sm:ml-0 3xl:ml-16 lg:-ml-12 md:-ml-4"
+                />
 
-            {/* Content */}
-            <div className="flex flex-col sm:flex-row items-start sm:items-center w-full sm:-ml-2 md:-ml-12 lg:ml-10 -mt-2 sm:-mt-10 ml-5 ">
-                {/* Order Label */}
-                <span className="text-[#E4E4E4] mr-0  3xl:ml-0 2xl:ml-0 xl:-ml-[3rem] lg:-ml-[3rem] text-base sm:text-sm md:text-sm 3xl:text-[20px] 2xl:text-[18px] xl:text-[17px] lg:text-[12px] md:text-[12px] mb-2 sm:mb-0 lg:whitespace-nowrap">
-                    Online place order
-                </span>
-
-                {/* Order Button */}
-                <button className="bg-[#FF4C15] text-white text-sm md:text-base 3xl:text-[20px] 2xl:text-[18px] xl:text-[17px]  lg:text-[12px] md:text-[12px] px-2 md:px-3 py-1 md:py-2 rounded-sm flex items-center font-semibold mb-2 sm:mb-0 lg:whitespace-nowrap 3xl:ml-4 3xl:mr-1 2xl:ml-4 2xl:mr-1 xl:ml-4 xl:mr-1 lg:ml-4 lg:mr-1 ">
-                   Order Now
-                    <ChevronRight className="ml-1 text-white" size={16} md:size={20} />
-                </button>
-
-                {/* Divider */}
-                <div className="hidden sm:block">
-                    <div className="flex items-center mx-0 sm:mx-1 md:mx-2 mb-2 sm:mb-0">
-                        <div className="h-3 w-[1px] md:w-[2px] md:h-4 bg-gray-400 mx-1 md:mx-2"></div>
-                    </div>
-                </div>
-
-                {/* Kitchen Availability */}
-                <div className="flex items-center space-x-1 sm:space-x-2 md:space-x-4 mr-0 sm:mr-2 md:mr-4 mb-2 sm:mb-0 lg:whitespace-nowrap">
-                    <img
-                        src={isKitchenOpen ? "/kitchen.svg" : "/public/kitchen-unavailable.svg"}
-                        alt="Kitchen status"
-                        className="h-4 w-4 md:h-5 md:w-5"
-                    />
-                    <span
-                        className={`text-sm 3xl:text-base 2xl:text-base xl:text-base lg:text-[12px] font-semibold ${
-                            isKitchenOpen ? 'text-[#2C6252]' : 'text-[#FF4C15]'
-                        }`}
-                    >
-                        {isKitchenOpen ? 'Kitchen available' : 'Kitchen unavailable'}
+                {/* Content */}
+                <div className="flex flex-col sm:flex-row items-start sm:items-center w-full sm:-ml-2 md:-ml-12 lg:ml-10 -mt-2 sm:-mt-10 ml-5 ">
+                    {/* Order Label */}
+                    <span className="text-[#E4E4E4] mr-0  3xl:ml-0 2xl:ml-0 xl:-ml-[3rem] lg:-ml-[3rem] text-base sm:text-sm md:text-sm 3xl:text-[20px] 2xl:text-[18px] xl:text-[17px] lg:text-[12px] md:text-[12px] mb-2 sm:mb-0 lg:whitespace-nowrap">
+                        Online place order
                     </span>
-                </div>
 
-                {/* Dynamic Clock */}
-                <div className="flex items-center space-x-1 sm:space-x-2 md:space-x-4 mr-0 sm:mr-2 md:mr-4 mb-2 sm:mb-0 lg:whitespace-nowrap">
-                    <AnalogClock />
-                    <span className="text-sm 3xl:text-base 2xl:text-base xl:text-base lg:text-[12px]  text-[#2C6252]  font-semibold">{formattedTime}</span>
-                </div>
+                    {/* Order Button */}
+                    <button className="bg-[#FF4C15] text-white text-sm md:text-base 3xl:text-[20px] 2xl:text-[18px] xl:text-[17px]  lg:text-[12px] md:text-[12px] px-2 md:px-3 py-1 md:py-2 rounded-sm flex items-center font-semibold mb-2 sm:mb-0 lg:whitespace-nowrap 3xl:ml-4 3xl:mr-1 2xl:ml-4 2xl:mr-1 xl:ml-4 xl:mr-1 lg:ml-4 lg:mr-1 ">
+                        Order Now
+                        <ChevronRight className="ml-1 text-white" size={16} md:size={20} />
+                    </button>
 
-                {/* Bottom Line */}
-                <div className="w-44 h-[1px] md:h-[1.5px] bg-[#FF4C15] ml-0 sm:ml-1 md:ml-2 overflow-hidden"></div>
+                    {/* Divider */}
+                    <div className="hidden sm:block">
+                        <div className="flex items-center mx-0 sm:mx-1 md:mx-2 mb-2 sm:mb-0">
+                            <div className="h-3 w-[1px] md:w-[2px] md:h-4 bg-gray-400 mx-1 md:mx-2"></div>
+                        </div>
+                    </div>
+
+                    {/* Kitchen Availability */}
+                    <div className="flex items-center space-x-1 sm:space-x-2 md:space-x-4 mr-0 sm:mr-2 md:mr-4 mb-2 sm:mb-0 lg:whitespace-nowrap">
+                        <img
+                            src={isKitchenOpen ? "/kitchen.svg" : "/kitchen-unavailable.svg"}
+                            alt="Kitchen status"
+                            className="h-4 w-4 md:h-5 md:w-5"
+                        />
+                        <span
+                            className={`text-sm 3xl:text-base 2xl:text-base xl:text-base lg:text-[12px] font-semibold ${isKitchenOpen ? 'text-[#2C6252]' : 'text-[#FF4C15]'
+                                }`}
+                        >
+                            {isKitchenOpen ? 'Kitchen available' : 'Kitchen unavailable'}
+                        </span>
+                    </div>
+
+                    {/* Dynamic Clock */}
+                    <div className="flex items-center space-x-1 sm:space-x-2 md:space-x-4 mr-0 sm:mr-2 md:mr-4 mb-2 sm:mb-0 lg:whitespace-nowrap">
+                        <AnalogClock />
+                        <span className="text-sm 3xl:text-base 2xl:text-base xl:text-base lg:text-[12px]  text-[#2C6252]  font-semibold">{formattedTime}</span>
+                    </div>
+
+                    {/* Bottom Line */}
+                    <div className="w-44 h-[1px] md:h-[1.5px] bg-[#FF4C15] ml-0 sm:ml-1 md:ml-2 overflow-hidden"></div>
+                </div>
             </div>
-        </div>
         </div>
     );
 };
