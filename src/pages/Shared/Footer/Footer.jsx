9e5495467726af4d5e8c8b0cb50805cf7ab5e26b
Neaz-mq <neazmorshed666@gmail.com>
2025-06-24 13:17:30 +0600

    footer updated for 2xl device

--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -13,9 +13,9 @@ const Footer = () => {
     <footer className="bg-[#2C6252] text-white">
       {/* Top Footer Content */}
       <Container>
-        <div className="px-6 py-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 3xl:gap-10 2xl:gap-6 xl:gap-4 lg:gap-10 text-center lg:text-left">
+        <div className="px-6 py-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 3xl:gap-10 2xl:gap-14 xl:gap-4 lg:gap-10 text-center lg:text-left">
           {/* Logo */}
-          <div className="flex justify-center 3xl:justify-center items-start w-full 3xl:ml-8 2xl:ml-1 2xl:justify-start xl:ml-1 xl:justify-start lg:justify-start">
+          <div className="flex justify-center 3xl:justify-center items-start w-full 3xl:ml-8 2xl:ml-20 2xl:justify-start xl:ml-1 xl:justify-start lg:justify-start">
             <a href="/">
               <div className="bg-[#3A7563] px-4 py-4 flex items-center space-x-2 w-fit 3xl:mt-2 2xl:mt-2 xl:mt-2 lg:mt-1 mx-auto">
                 <img
@@ -91,7 +91,7 @@ const Footer = () => {
       {/* Divider Line */}
       <div className="w-full flex justify-center">
         <Container>
-          <div className="border-t border-[#ACBCB8] 3xl:w-[80rem] 2xl:w-[69rem] xl:w-[62rem] lg:w-[49rem] mt-6 mb-12 3xl:ml-36 2xl:ml-7 xl:ml-7 lg:ml-7"></div>
+          <div className="border-t border-[#ACBCB8] 3xl:w-[80rem] 2xl:w-[62rem] xl:w-[62rem] lg:w-[49rem] mt-6 mb-12 3xl:ml-36 2xl:ml-28 xl:ml-7 lg:ml-7"></div>
         </Container>
       </div>
 
@@ -99,7 +99,7 @@ const Footer = () => {
       {/* Bottom Bar */}
       <div className="bg-[#FF4C15] w-full flex justify-center">
         <Container>
-          <div className="flex flex-col md:flex-row justify-center md:justify-between items-center px-6 py-4 text-sm text-white gap-4 3xl:ml-28 2xl:ml-0 xl:ml-0">
+          <div className="flex flex-col md:flex-row justify-center md:justify-between items-center px-6 py-4 text-sm text-white gap-4 3xl:ml-28 2xl:ml-20 xl:ml-0">
             <div className="flex items-center gap-2">
               <FaRegCopyright />
               <span className="whitespace-nowrap ">
