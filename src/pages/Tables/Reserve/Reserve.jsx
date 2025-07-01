066d2a0e2e4bd07ef6bf2277e41f06ef9640f466
Neaz-mq <neazmorshed666@gmail.com>
2025-07-01 16:46:38 +0600

    reserve updated for all devices

--- src/pages/Tables/Reserve/Reserve.jsx
+++ src/pages/Tables/Reserve/Reserve.jsx
@@ -5,18 +5,18 @@ const tableData = [
   { label: "T-2", booked: true },
   { label: "T-3", booked: false },
   { label: "T-4", booked: false },
-  { label: "T-5", booked: true },
-  { label: "B-1", booked: true },
+  { label: "T-5", booked: false },
+  { label: "B-1", booked: false },
   { label: "B-2", booked: false },
   { label: "B-3", booked: true },
   { label: "B-4", booked: false },
-  { label: "B-5", booked: false },
+  { label: "B-5", booked: true },
 ];
 
 const Reserve = () => {
   return (
     <Container>
-    <div className="3xl:px-16 mb-24">
+    <div className="3xl:px-16 2xl:px-5 xl:px-14 lg:px-2 mb-24 -mt-14">
           {/* Heading */}
       <div className="text-center lg:text-left mb-10 mt-20 ">
         <h2 className="text-2xl lg:text-4xl font-bold text-[#2C6252] leading-relaxed">
@@ -35,7 +35,7 @@ const Reserve = () => {
         {tableData.map((table, index) => (
           <div
             key={index}
-            className={`w-44 h-44 flex items-center justify-center text-white font-bold text-6xl  ${
+            className={`3xl:w-44 3xl:h-44 2xl:w-44 2xl:h-44 xl:w-40 xl:h-40 lg:w-28 lg:h-28 flex items-center justify-center text-white font-bold 3xl:text-6xl 2xl:text-6xl xl:text-5xl lg:text-4xl  ${
               table.booked ? "bg-[#FF4C15]" : "bg-[#2C6252]"
             }`}
           >
