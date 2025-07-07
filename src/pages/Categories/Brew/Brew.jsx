b3fb5bf604889a8f7c2a4218082e452d37490256
Neaz-mq <neazmorshed666@gmail.com>
2025-07-07 15:09:45 +0600

    order page updated for xl device

--- src/pages/Categories/Brew/Brew.jsx
+++ src/pages/Categories/Brew/Brew.jsx
@@ -5,7 +5,7 @@ import { motion as Motion } from "framer-motion";
 const Brew = () => {
   return (
     <Container>
-      <div className="3xl:px-16 2xl:px-6 xl:px-16 lg:px-2 3xl:mb-72 2xl:mb-72 xl:mb-[24rem] lg:mb-[22rem] 3xl:-mt-28">
+      <div className="3xl:px-16 2xl:px-6 xl:px-16 lg:px-2 3xl:mb-72 2xl:mb-72 xl:mb-[24rem] lg:mb-[22rem] 3xl:-mt-28 2xl:-mt-10 xl:mt-24">
         <div className="flex flex-col lg:flex-row items-center justify-between gap-12 relative">
           {/* Left Section */}
           <div className="relative max-w-2xl w-full text-center lg:text-left">
