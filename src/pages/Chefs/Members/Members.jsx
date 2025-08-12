a9a59025071e25271da43fc6cb769a5ea8fec583
Neaz-mq <neazmorshed666@gmail.com>
2025-08-12 13:19:30 +0600

    update the members

--- src/pages/Chefs/Members/Members.jsx
+++ src/pages/Chefs/Members/Members.jsx
@@ -40,22 +40,22 @@ const Members = () => {
     return (
         <Container>
             <section
-                className="py-8 md:py-12 3xl:px-16 2xl:px-20 xl:px-44 lg:px-0 md:px-0 sm:px-0 3xl:mt-36 2xl:mt-32 xl:mt-32 lg:mt-24 md:mt-20 sm:mt-14 3xl:mb-36 2xl:mb-40 xl:mb-28 lg:mb-28 sm:-ml-[15.5rem] 3xl:-ml-0 2xl:-ml-0 xl:-ml-0 lg:-ml-0 md:-ml-0 sm:w-[20rem] 3xl:w-full 2xl:w-full xl:w-full lg:w-full md:w-full "
+                className="py-8 md:py-12 3xl:px-16 2xl:px-20 xl:px-44 lg:px-0 md:px-0 sm:px-0 3xl:mt-36 2xl:mt-32 xl:mt-32 lg:mt-24 md:mt-20 sm:mt-14 3xl:mb-36 2xl:mb-40 xl:mb-28 lg:mb-28 sm:-ml-[10.5rem]  3xl:-ml-0 2xl:-ml-0 xl:-ml-0 lg:-ml-0 md:-ml-0 sm:w-full 3xl:w-full 2xl:w-full xl:w-full lg:w-full md:w-full "
                 aria-label="Meet Our Team"
             >
                 {/* Header Section */}
                 <Motion.div
-                    className="mb-10 md:mb-16 text-left flex justify-end items-center sm:ml-[7.4rem] 3xl:ml-0 2xl:ml-0 xl:ml-0 md:ml-0 sm:w-60 3xl:w-full 2xl:w-full xl:w-full lg:w-full md:w-full"
+                    className="mb-10 md:mb-16 text-left flex justify-end items-center  3xl:ml-0 2xl:ml-0 xl:ml-0 md:ml-0 sm:ml-28 sm:w-40    3xl:w-full 2xl:w-full xl:w-full lg:w-full md:w-full "
                     initial={{ opacity: 0, y: 30 }}
                     whileInView={{ opacity: 1, y: 0 }}
                     transition={{ duration: 0.6, ease: "easeOut" }}
                     viewport={{ once: true }}
                 >
-                    <h1 className="text-3xl 3xl:text-5xl 2xl:text-4xl xl:text-2xl lg:text-2xl md:text-2xl sm:text-sm font-bold text-[#2C6252] mb-2 leading-relaxed 3xl:mr-8 2xl:mr-16 xl:-mr-16 lg:mr-20 md:mr-36 sm:mr-0 items-start ">
+                    <h1 className="text-3xl 3xl:text-5xl 2xl:text-4xl xl:text-2xl lg:text-2xl md:text-2xl sm:text-sm font-bold text-[#2C6252] mb-2 leading-relaxed 3xl:mr-8 2xl:mr-16 xl:-mr-16 lg:mr-20 md:mr-36 sm:-mr-0 items-start  ">
                         Our expertise all team <br className="hidden md:block" />
                         <span className="inline-flex items-center leading-relaxed">
                             members
-                            <span className="bg-[#FF4C15] text-white 3xl:text-sm 2xl:text-sm xl:text-sm lg:text-[10px] md:text-[12px] sm:text-[8px] 3xl:px-4 3xl:py-2 2xl:px-4 2xl:py-2 xl:px-4 xl:py-2 md:px-4 md:py-2 sm:px-2 sm:py-1 flex items-center space-x-2 ml-4">
+                            <span className="bg-[#FF4C15] text-white 3xl:text-sm 2xl:text-sm xl:text-sm lg:text-[10px] md:text-[12px] sm:text-[8px] 3xl:px-4 3xl:py-2 2xl:px-4 2xl:py-2 xl:px-4 xl:py-2 md:px-4 md:py-2 sm:px-2 sm:py-1 flex items-center space-x-2 ml-4 ">
                                 <svg
                                     className="w-4 h-4"
                                     fill="currentColor"
@@ -79,12 +79,12 @@ const Members = () => {
 
                     {/* Left Image Area */}
                     <Motion.figure
-                        className="flex justify-end relative 3xl:left-2 2xl:-left-14 xl:-left-[7.5rem] lg:left-2 md:left-4 sm:left-[7.4rem] bg-[#3F7765] 3xl:h-[99%] 3xl:w-[75%] 2xl:h-[99.8%] 2xl:w-[80%] xl:h-[99.8%] xl:w-[94%] lg:h-[99.8%] lg:w-[95%] md:w-[94%] sm:w-[90%] "
+                        className="flex justify-end relative 3xl:left-2 2xl:-left-14 xl:-left-[7.5rem] lg:left-2 md:left-4 sm:left-[2.7rem] bg-[#3F7765] 3xl:h-[99%] 3xl:w-[75%] 2xl:h-[99.8%] 2xl:w-[80%] xl:h-[99.8%] xl:w-[94%] lg:h-[99.8%] lg:w-[95%] md:w-[94%] sm:w-[97%] sm:h-auto "
                         role="img"
                         aria-label="Featured Chef"
                     >
                         <Motion.div
-                            className="3xl:-ml-16 3xl:-mr-[22.4rem] 3xl:-mt-32 2xl:-ml-60 2xl:-mr-[20rem] xl:-ml-36 xl:-mr-[27rem] 2xl:-mt-36 xl:-mt-48 lg:-ml-44 lg:-mr-[7rem] lg:-mt-48"
+                            className="3xl:-ml-16 3xl:-mr-[22.4rem] 3xl:-mt-32 2xl:-ml-60 2xl:-mr-[20rem] xl:-ml-36 xl:-mr-[27rem] 2xl:-mt-36 xl:-mt-48 lg:-ml-44 lg:-mr-[7rem] lg:-mt-48 "
                             initial={{ y: 100, opacity: 0 }}
                             whileInView={{ y: 0, opacity: 1 }}
                             transition={{
@@ -97,7 +97,7 @@ const Members = () => {
                             <img
                                 src="https://res.cloudinary.com/dxohwanal/image/upload/v1752059008/chef4_pgbdux.webp"
                                 alt="Portrait of happy male chef in uniform"
-                                className="object-contain h-auto w-full 3xl:-ml-2 2xl:ml-6 xl:-ml-16 lg:ml-4 xl:mt-[5.93rem] 3xl:mt-0 2xl:mt-0 lg:mt-[5.93rem]"
+                                className="object-contain h-auto 3xl:w-full 2xl:w-full xl:w-full lg:w-full md:w-full sm:w-52 3xl:-ml-2 2xl:ml-6 xl:-ml-16 lg:ml-4 sm:ml-12 xl:mt-[5.93rem] 3xl:mt-0 2xl:mt-0 lg:mt-[5.93rem]"
                                 onError={(e) => {
                                     e.target.onerror = null;
                                     e.target.src = "https://placehold.co/400x600/A0A0A0/FFFFFF?text=Image+Error";
@@ -108,7 +108,7 @@ const Members = () => {
 
                     {/* Right - Team Cards */}
                     <Motion.div
-                        className="grid grid-cols-1  md:grid-cols-2 3xl:gap-x-20 3xl:gap-y-20 2xl:gap-x-36 2xl:gap-y-20 xl:gap-x-40 xl:gap-y-24 lg:gap-x-5 lg:gap-y-12 md:gap-x-0 md:gap-y-24 sm:gap-x-0 sm:gap-y-16 3xl:ml-20 2xl:ml-0 xl:ml-10 lg:-ml-1 md:ml-20 sm:ml-32 md:mt-16 3xl:mt-0 2xl:mt-0 xl:mt-0 lg:mt-0 sm:mt-20 sm:w-[13rem] 3xl:w-full 2xl:w-full xl:w-full lg:w-full md:w-[30rem]  "
+                        className="grid grid-cols-1  md:grid-cols-2 3xl:gap-x-20 3xl:gap-y-20 2xl:gap-x-36 2xl:gap-y-20 xl:gap-x-40 xl:gap-y-24 lg:gap-x-5 lg:gap-y-12 md:gap-x-0 md:gap-y-24 sm:gap-x-0 sm:gap-y-16 3xl:ml-20 2xl:ml-0 xl:ml-10 lg:-ml-1 md:ml-20 sm:ml-24 md:mt-16 3xl:mt-0 2xl:mt-0 xl:mt-0 lg:mt-0 sm:mt-20 sm:w-[10rem] 3xl:w-full 2xl:w-full xl:w-full lg:w-full md:w-[30rem]  "
                         initial="hidden"
                         whileInView="visible"
                         variants={{
@@ -125,7 +125,7 @@ const Members = () => {
                         {teamMembers.map((member) => (
                             <Motion.article
                                 key={member.id}
-                                className="relative bg-white p-4 flex flex-col justify-end items-center text-center 3xl:h-[200px] 3xl:w-[250px] 2xl:h-[200px] 2xl:w-[220px] xl:h-[200px] xl:w-[170px] lg:h-[150px] lg:w-[180px] md:h-[180px] md:w-[180px] sm:h-[130px] sm:w-[130px] border border-gray-400"
+                                className="relative bg-white p-4 flex flex-col justify-end items-center text-center 3xl:h-[200px] 3xl:w-[250px] 2xl:h-[200px] 2xl:w-[220px] xl:h-[200px] xl:w-[170px] lg:h-[150px] lg:w-[180px] md:h-[180px] md:w-[180px] sm:h-[120px] sm:w-[180px] border border-gray-400"
                                 initial={{ opacity: 0, y: 40 }}
                                 animate={{ opacity: 1, y: 0 }}
                                 transition={{ duration: 0.6, ease: "easeOut" }}
@@ -150,7 +150,7 @@ const Members = () => {
                                         <path d="M10 2a8 8 0 100 16 8 8 0 000-16zM5.3 8.3a1 1 0 011.4 0L10 11.6l3.3-3.3a1 1 0 111.4 1.4l-4 4a1 1 0 01-1.4 0l-4-4a1 1 0 010-1.4z"></path>
                                     </svg>
                                 </div>
-                                <figcaption className="absolute 3xl:-left-8 2xl:-left-4 xl:-left-4 lg:-left-3 md:-left-3 sm:-left-3 top-28 -translate-y-1/2 -rotate-90 origin-left 3xl:text-lg 2xl:text-lg xl:text-lg lg:text-sm font-semibold text-[#2C6252] whitespace-nowrap z-20">
+                                <figcaption className="absolute 3xl:-left-8 2xl:-left-4 xl:-left-4 lg:-left-3 md:-left-3 sm:-left-3 top-28 3xl:-translate-y-1/2 2xl:-translate-y-1/2 xl:-translate-y-1/2 lg:-translate-y-1/2 md:-translate-y-1/2 sm:-translate-y-8 -rotate-90 origin-left 3xl:text-lg 2xl:text-lg xl:text-lg lg:text-sm sm:text-xs font-semibold text-[#2C6252] whitespace-nowrap z-20">
                                     - {member.name}
                                 </figcaption>
                                 <div className="relative z-10 w-full mt-auto flex flex-col items-center text-gray-700 bg-white bg-opacity-75 rounded-md px-2 py-1">
